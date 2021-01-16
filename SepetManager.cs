9ae096dfb9181bf4b70208109f62c697c2de51a3
mX <mX@DESKTOP-JIK5ITT>
2021-01-16 23:54:18 +0300

    Add project files.

---
+++ SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {   
+        //naming convencion
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : "+urun.Adi);
+        }
+        public void Ekle2(string urunAdi,string aciklama,double fiyat,int stok) 
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
